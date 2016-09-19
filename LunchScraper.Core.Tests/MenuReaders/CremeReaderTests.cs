40cd343f2aeb4d23eac77521b4d432d24d69d81d
Peter <palle.ursvik@gmail.com>
2016-09-19 22:04:07 +0200

    CremeReaderTests CanParseWeek38

--- LunchScraper.Core.Tests/MenuReaders/CremeReaderTests.cs
+++ LunchScraper.Core.Tests/MenuReaders/CremeReaderTests.cs
@@ -26,5 +26,23 @@ namespace LunchScraper.Core.Tests.MenuReaders
 			Assert.IsTrue(dishes.Any(d => d.Description == "soja och honungsglacerade kycklinglårfiléer med krispiga grönsaker, nudelsallad och korianderdipp" && d.Date.DayOfWeek == DayOfWeek.Thursday));
 			Assert.IsTrue(dishes.Any(d => d.Description == "Gärdets rosagrillade picanha \"black and white\" med ljummen potatissallad, rödvinssås och bearnaise" && d.Date.DayOfWeek == DayOfWeek.Friday));
 		}
+
+		[TestMethod]
+		public void ReadWeeklyMeny_CanParseWeek38()
+		{
+			// Arrange
+			var scraper = TestHelper.MockWebScraper(@"TestInput\CremeV38.htm");
+			var menuReader = new CremeReader(scraper);
+
+			// Act
+			var dishes = menuReader.ReadWeeklyMenu();
+
+			// Assert
+			Assert.IsTrue(dishes.Any(d => d.Description == "Nattbakad fläskkarré med rökig glaze, rostad potatis och avocadocreme" && d.Date.DayOfWeek == DayOfWeek.Monday));
+			Assert.IsTrue(dishes.Any(d => d.Description == "Halstrad regnbågsfilé med hollandaise, citron, grönsaksjulienne och gräslöksslungad färskpotatis" && d.Date.DayOfWeek == DayOfWeek.Tuesday));
+			Assert.IsTrue(dishes.Any(d => d.Description == "Wallenbergare med ärtor, lingon, skirat smör och potatispure" && d.Date.DayOfWeek == DayOfWeek.Wednesday));
+			Assert.IsTrue(dishes.Any(d => d.Description == "Tandorikyckling med gurksallad, basmatiris , grönsaker och myntayoghurt" && d.Date.DayOfWeek == DayOfWeek.Thursday));
+			Assert.IsTrue(dishes.Any(d => d.Description == "Gärdets rosagrillade picanha \"black and white\" med ljummen potatissallad, rödvinssås och bearnaise" && d.Date.DayOfWeek == DayOfWeek.Friday));
+		}
 	}
 }
