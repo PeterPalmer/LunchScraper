e5831607f4fca92eb16343e298bd2f9d13d282b4
Peter <palle.ursvik@gmail.com>
2017-11-19 01:32:42 +0100

    Slide menus in.

--- LunchScraper.Web/Scripts/ScraperMain.js
+++ LunchScraper.Web/Scripts/ScraperMain.js
@@ -1,12 +1,27 @@
 ﻿
-function ShowDay(id) {
+function ShowDay(id, isPreviousDay) {
 	for (var i = 0; i < 7; i++) {
 		document.getElementById("header" + i).style.display = "none";
 		document.getElementById("body" + i).style.display = "none";
 	}
 
-	document.getElementById("header" + id).style.display = "block";
-	document.getElementById("body" + id).style.display = "block";
+	var header = document.getElementById("header" + id);
+	var body = document.getElementById("body" + id);
+
+	header.classList.remove('slideRight', 'slideLeft');
+	body.classList.remove('slideRight', 'slideLeft');
+
+	header.style.display = "block";
+	body.style.display = "block";
+
+	if (isPreviousDay) {
+		header.classList.add('slideRight');
+		body.classList.add('slideRight');
+	}
+	else {
+		header.classList.add('slideLeft');
+		body.classList.add('slideLeft');
+	}
 }
 
 function ToggleExceptionBody() {
@@ -27,7 +42,7 @@ function onSwipeLeft() {
 	if (currentDayId > 5) {
 		currentDayId = 1;
 	}
-	ShowDay(currentDayId);
+	ShowDay(currentDayId, false);
 }
 
 function onSwipeRight() {
@@ -35,7 +50,7 @@ function onSwipeRight() {
 	if (currentDayId < 1) {
 		currentDayId = 5;
 	}
-	ShowDay(currentDayId);
+	ShowDay(currentDayId, true);
 }
 
 function detectSwipes() {
@@ -44,45 +59,58 @@ function detectSwipes() {
 	document.addEventListener('touchend', handleTouchEnd, false);
 }
 
+
 var xDown = null;
 var yDown = null;
 var startTime;
 var elapsedTime;
+var swipeDirection;
+var horizontal = 1;
+var vertical = 2;
 
 function handleTouchStart(evt) {
 	xDown = evt.touches[0].pageX;
 	yDown = evt.touches[0].pageY;
 	startTime = new Date().getTime();
+	swipeDirection = null;
 }
 
 function handleTouchMove(evt) {
 	if (!xDown) return;
 
+	if (!swipeDirection) {
+		if (Math.abs(getSwipeDistanceX(evt.touches)) > Math.abs(getSwipeDistanceY(evt.touches))) {
+			swipeDirection = horizontal;
+		}
+		else {
+			swipeDirection = vertical;
+		}
+	}
+
+	if (swipeDirection == vertical) return;
+
 	var xDiff = getSwipeDistanceX(evt.touches);
 	var offset = Math.min(Math.max(xDiff, -300), 300);
-	//document.getElementById("header" + currentDayId).style.marginLeft = offset + "px";
 	document.getElementsByTagName("body")[0].style.marginLeft = offset + "px";
 	document.getElementsByTagName("body")[0].style.marginRight = (-1 * offset) + "px";
 }
 
 function handleTouchEnd(evt) {
-	//document.getElementById("header" + currentDayId).style.marginLeft = "0";
+	var xDiff = getSwipeDistanceX(evt.changedTouches);
+	xDown = null;
+	yDown = null;
+
 	document.getElementsByTagName("body")[0].style.marginLeft = "0";
 	document.getElementsByTagName("body")[0].style.marginRight = "0";
 
 	elapsedTime = new Date().getTime() - startTime;
 	if (elapsedTime <= 100) return;
 
-	var xDiff = getSwipeDistanceX(evt.changedTouches);
-	var yDiff = getSwipeDistanceY(evt.changedTouches);
-	xDown = null;
-	yDown = null;
-
-	if (Math.abs(yDiff) > 100) {
+	if (swipeDirection == vertical) {
 		return;
 	}
 
-	if (Math.abs(xDiff) < 150) {
+	if (Math.abs(xDiff) < 100) {
 		return;
 	}
 
